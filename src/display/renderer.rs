8415e2f2e0eee1de38744a09aff0be08462bcb33
Valerian Saliou <valerian@valeriansaliou.name>
2020-04-26 19:08:48 +0200

    Fix code style
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/display/renderer.rs
+++ src/display/renderer.rs
@@ -10,16 +10,18 @@ use plotters::prelude::*;
 use telemetry::structures::MachineStateSnapshot;
 
 use crate::config::environment::{
-    DISPLAY_GRAPH_OFFSET_HEIGHT, DISPLAY_GRAPH_OFFSET_WIDTH, DISPLAY_WINDOW_SIZE_HEIGHT,
-    DISPLAY_WINDOW_SIZE_WIDTH, GRAPH_DRAW_LABEL_WIDTH, GRAPH_DRAW_LINE_SIZE, GRAPH_DRAW_MARGIN,
-    GRAPH_DRAW_RANGE_HIGH, GRAPH_DRAW_RANGE_LOW, GRAPH_DRAW_SECONDS,
+    BRANDING_HEIGHT, BRANDING_WIDTH, DISPLAY_GRAPH_OFFSET_HEIGHT, DISPLAY_GRAPH_OFFSET_WIDTH,
+    DISPLAY_WINDOW_SIZE_HEIGHT, DISPLAY_WINDOW_SIZE_WIDTH, GRAPH_DRAW_LABEL_WIDTH,
+    GRAPH_DRAW_LINE_SIZE, GRAPH_DRAW_MARGIN_BOTTOM, GRAPH_DRAW_MARGIN_LEFT,
+    GRAPH_DRAW_MARGIN_RIGHT, GRAPH_DRAW_MARGIN_TOP, GRAPH_DRAW_RANGE_HIGH, GRAPH_DRAW_RANGE_LOW,
+    GRAPH_DRAW_SECONDS,
 };
 
 use crate::chip::ChipState;
 use crate::physics::types::DataPressure;
 
 use super::fonts::Fonts;
-use super::screen::{Ids, Screen};
+use super::screen::{Ids, Screen, ScreenDataBranding, ScreenDataGraph};
 use super::support::GliumDisplayWinitWrapper;
 
 pub struct DisplayRendererBuilder;
@@ -31,6 +33,7 @@ pub struct DisplayRenderer {
 const GRAPH_WIDTH: u32 = DISPLAY_WINDOW_SIZE_WIDTH - DISPLAY_GRAPH_OFFSET_WIDTH;
 const GRAPH_HEIGHT: u32 = DISPLAY_WINDOW_SIZE_HEIGHT - DISPLAY_GRAPH_OFFSET_HEIGHT;
 
+#[allow(clippy::new_ret_no_self)]
 impl DisplayRendererBuilder {
     pub fn new(fonts: Fonts) -> DisplayRenderer {
         DisplayRenderer { fonts }
@@ -51,7 +54,6 @@ impl DisplayRenderer {
         // The `WidgetId` for our background and `Image` widgets.
         let ids = Ids::new(interface.widget_id_generator());
 
-        // .clone() makes the borrow checker happy
         match chip_state {
             ChipState::WaitingData => self.empty(ids, interface, image_map),
             ChipState::Stopped => self.stopped(ids, interface, image_map),
@@ -123,7 +125,67 @@ impl DisplayRenderer {
         data_pressure: &DataPressure,
         machine_snapshot: &MachineStateSnapshot,
     ) -> conrod_core::image::Map<texture::Texture2d> {
-        // Create chart
+        // Create branding
+        let branding_image_texture = self.draw_branding(display);
+        let (branding_width, branding_height) = (
+            branding_image_texture.get_width(),
+            branding_image_texture.get_height().unwrap(),
+        );
+        let branding_image_id = image_map.insert(branding_image_texture);
+
+        // Create graph
+        let graph_image_texture = self.draw_data_chart(data_pressure, display);
+        let (graph_width, graph_height) = (
+            graph_image_texture.get_width(),
+            graph_image_texture.get_height().unwrap(),
+        );
+        let graph_image_id = image_map.insert(graph_image_texture);
+
+        // Create widgets
+        let ui = interface.set_widgets();
+
+        let mut screen = Screen::new(ui, &ids, &self.fonts, Some(machine_snapshot));
+
+        screen.render_with_data(
+            ScreenDataBranding {
+                image_id: branding_image_id,
+                width: branding_width as _,
+                height: branding_height as _,
+            },
+            ScreenDataGraph {
+                image_id: graph_image_id,
+                width: graph_width as _,
+                height: graph_height as _,
+            },
+        );
+
+        image_map
+    }
+
+    fn draw_branding(&self, display: &GliumDisplayWinitWrapper) -> glium::texture::Texture2d {
+        let buffer = vec![0; (BRANDING_WIDTH * BRANDING_HEIGHT * 4) as usize];
+
+        // TODO: fill buffer
+
+        // Create image from raw buffer
+        let rgba_image: RgbaImage = RgbImage::from_raw(BRANDING_WIDTH, BRANDING_HEIGHT, buffer)
+            .unwrap()
+            .convert();
+        let image_dimensions = rgba_image.dimensions();
+
+        let raw_image = glium::texture::RawImage2d::from_raw_rgba_reversed(
+            &rgba_image.into_raw(),
+            image_dimensions,
+        );
+
+        glium::texture::Texture2d::new(&display.0, raw_image).unwrap()
+    }
+
+    fn draw_data_chart(
+        &self,
+        data_pressure: &DataPressure,
+        display: &GliumDisplayWinitWrapper,
+    ) -> glium::texture::Texture2d {
         let mut buffer = vec![0; (GRAPH_WIDTH * GRAPH_HEIGHT * 4) as usize];
 
         // Docs: https://docs.rs/plotters/0.2.12/plotters/drawing/struct.BitMapBackend.html
@@ -136,7 +198,10 @@ impl DisplayRenderer {
 
         // Docs: https://docs.rs/plotters/0.2.12/plotters/chart/struct.ChartBuilder.html
         let mut chart = ChartBuilder::on(&root)
-            .margin(GRAPH_DRAW_MARGIN)
+            .margin_top(GRAPH_DRAW_MARGIN_TOP)
+            .margin_bottom(GRAPH_DRAW_MARGIN_BOTTOM)
+            .margin_left(GRAPH_DRAW_MARGIN_LEFT)
+            .margin_right(GRAPH_DRAW_MARGIN_RIGHT)
             .x_label_area_size(0)
             .y_label_area_size(GRAPH_DRAW_LABEL_WIDTH)
             .build_ranged(
@@ -159,13 +224,12 @@ impl DisplayRenderer {
 
         // Docs: https://docs.rs/plotters/0.2.12/plotters/prelude/struct.LineSeries.html
         chart
-            .draw_series(
-                LineSeries::new(
-                    data_pressure.iter().map(|x| (x.0, x.1 as i32)),
-                    ShapeStyle::from(&plotters::style::RGBColor(0, 137, 255)).filled(),
-                )
-                .point_size(GRAPH_DRAW_LINE_SIZE),
-            )
+            .draw_series(LineSeries::new(
+                data_pressure.iter().map(|x| (x.0, x.1 as i32)),
+                ShapeStyle::from(&plotters::style::RGBColor(0, 137, 255))
+                    .filled()
+                    .stroke_width(GRAPH_DRAW_LINE_SIZE),
+            ))
             .unwrap();
 
         drop(chart);
@@ -179,34 +243,12 @@ impl DisplayRenderer {
             .unwrap()
             .convert();
         let image_dimensions = rgba_image.dimensions();
+
         let raw_image = glium::texture::RawImage2d::from_raw_rgba_reversed(
             &rgba_image.into_raw(),
             image_dimensions,
         );
-        let image_texture = glium::texture::Texture2d::new(&display.0, raw_image).unwrap();
 
-        let (w, h) = (
-            image_texture.get_width(),
-            image_texture.get_height().unwrap(),
-        );
-
-        let image_id = image_map.insert(image_texture);
-
-        // Create widgets
-        let ui = interface.set_widgets();
-
-        let mut screen = Screen::new(ui, &ids, &self.fonts, Some(machine_snapshot));
-
-        screen.render_with_data(image_id, w as _, h as _);
-
-        image_map
-    }
-
-    fn draw_data_chart() {
-        // TODO: move chart drawer code block there
-    }
-
-    fn draw_data_widgets() {
-        // TODO: move widgets drawer code block there
+        glium::texture::Texture2d::new(&display.0, raw_image).unwrap()
     }
 }
