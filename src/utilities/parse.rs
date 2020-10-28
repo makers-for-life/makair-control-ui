255f48a294a7f038507a0625aa39a4def2799e77
Valerian Saliou <valerian@valeriansaliou.name>
2020-10-28 14:12:14 +0100

    Satisfy this grumpy clippy
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/utilities/parse.rs
+++ src/utilities/parse.rs
@@ -7,7 +7,7 @@ pub fn parse_version_number(version: &str) -> &str {
     // The firmware may sometimes send a version number starting with a "v" or "V" character, \
     //   which we do not want here, hence why we need to filter this out.
     if version.len() > 1 {
-        match version.chars().nth(0) {
+        match version.chars().next() {
             Some('v') | Some('V') => {
                 // Clear out the 'v' indicator from the version number
                 return &version[1..];
