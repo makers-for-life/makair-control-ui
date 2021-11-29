952e7f1d12453febc85e972ac078215a8d71c955
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2021-11-29 13:25:55 +0100

    Resize only if scaling factor is different of 1

--- src/utilities/image.rs
+++ src/utilities/image.rs
@@ -13,29 +13,37 @@ pub fn reverse_rgba(image: &[u8], width: u32, height: u32) -> Vec<u8> {
     // Reverses an image over the Y axis, so that it is displayed on screen correctly, as the \
     //   renderer works on an inverted Y axis.
 
-    let (w1, h1) = (width / FACTOR, height / FACTOR);
-    let (w2, h2) = (width, height);
-    let src = (image).as_rgba();
+    if FACTOR == 1 {
+        image
+            .chunks((width as usize) * 4)
+            .rev()
+            .flat_map(|row| row.iter())
+            .copied()
+            .collect()
+    } else {
+        let (w1, h1) = (width / FACTOR, height / FACTOR);
+        let (w2, h2) = (width, height);
+        let src = (image).as_rgba();
 
-    let mut dst = vec![RGBA::new(0, 0, 0, 0); (w2 * h2).try_into().unwrap()];
-    // Create resizer instance.
-    let mut resizer = resize::new(
-        w1.try_into().unwrap(),
-        h1.try_into().unwrap(),
-        w2.try_into().unwrap(),
-        h2.try_into().unwrap(),
-        RGBA8,
-        Mitchell,
-    )
-    .unwrap();
-    resizer.resize(&src, &mut dst);
+        let mut dst = vec![RGBA::new(0, 0, 0, 0); (w2 * h2).try_into().unwrap()];
+        // Create resizer instance.
+        let mut resizer = resize::new(
+            w1.try_into().unwrap(),
+            h1.try_into().unwrap(),
+            w2.try_into().unwrap(),
+            h2.try_into().unwrap(),
+            RGBA8,
+            Mitchell,
+        )
+        .unwrap();
+        resizer.resize(&src, &mut dst);
 
-    let export: Vec<u8> = dst.iter().flat_map(|rgba| rgba.iter()).collect();
-
-    export
-        .chunks((width as usize) * 4)
-        .rev()
-        .flat_map(|row| row.iter())
-        .copied()
-        .collect()
+        let export: Vec<u8> = dst.iter().flat_map(|rgba| rgba.iter()).collect();
+        export
+            .chunks((width as usize) * 4)
+            .rev()
+            .flat_map(|row| row.iter())
+            .copied()
+            .collect()
+    }
 }
